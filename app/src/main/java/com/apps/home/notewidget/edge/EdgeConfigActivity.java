c30325f5efedd597b280bf61580c20db23191ba7
k.kaszubski <kamcio181@o2.pl>
2016-08-18 16:59:19 +0200

    Config Activity in progress

--- app/src/main/java/com/apps/home/notewidget/edge/EdgeConfigActivity.java
+++ app/src/main/java/com/apps/home/notewidget/edge/EdgeConfigActivity.java
@@ -8,13 +8,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.apps.home.notewidget.R;
+import com.apps.home.notewidget.objects.Note;
 import com.apps.home.notewidget.utils.Constants;
 import com.apps.home.notewidget.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class EdgeConfigActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
     private static final String TAG = "EdgeConfigActivity";
@@ -51,6 +60,78 @@ public class EdgeConfigActivity extends AppCompatActivity implements CompoundBut
     protected void onStop() {
         super.onStop();
 
+        //TODO save checked items to prefs
+
         Utils.updateAllEdgePanels(this);
+    }
+}
+class CheckableItemsAdapter extends RecyclerView.Adapter<CheckableItemsAdapter.ViewHolder> { //TODO saveCheckedArray onStop
+    private static final String TAG = "CheckableItemsAdapter";
+    private static ArrayList<Note> notes;
+    private static boolean[] checkedArray;
+    private static String[] contents;
+
+    public CheckableItemsAdapter(ArrayList<Note> notes, String checked) {//checked format ";int;int;int;"
+        CheckableItemsAdapter.notes = notes;
+
+        checkedArray = new boolean[notes.size()];
+        for(int i = 0; i < checkedArray.length; i++) {
+            checkedArray[i] = checked.contains(";" + notes.get(i).getId() + ";");
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.double_line_check_recycle_view_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.titleTV.setText(note.getTitle());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(note.getCreatedAt());
+        holder.contentTV.setText(String.format("%1$tb %1$te, %1$tY %1$tT", calendar));
+        holder.checkBox.setChecked(checkedArray[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public CheckBox checkBox;
+        public TextView titleTV;
+        public TextView contentTV;
+
+        public ViewHolder(final View itemView) {
+            super(itemView);
+            titleTV = (TextView) itemView.findViewById(R.id.textView2);
+            contentTV = (TextView) itemView.findViewById(R.id.textView3);
+            checkBox = (CheckBox) itemView.findViewById(R.id.checkBox2);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkedArray[getLayoutPosition()] = !checkedArray[getLayoutPosition()];
+                    CheckableItemsAdapter.this.notifyItemChanged(getLayoutPosition());
+                }
+            });
+        }
+    }
+
+    public String getChekedNotes () {
+        StringBuilder builder = new StringBuilder();
+        builder.append(";");
+        for(int i = 0; i<checkedArray.length; i++){
+            if(checkedArray[i]){
+                builder.append(notes.get(i).getId()).append(";");
+            }
+        }
+        String result = builder.toString();
+        if(result.length()>2)
+            return result;
+        return null;
     }
 }
